8d0ed9cc35bc69cd53eb94e881e3ace3b8be56bf
haticevarlik <haticevarlik343@gmail.com>
2025-02-15 12:04:23 +0300

    wall done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,6 +10,18 @@ public class Main {
         System.out.println("LastName: " + person.getLastName());
 
         System.out.println("Age: " + person.getAge());
+
+        Wall wall = new Wall(5,4);
+
+        System.out.println("area= " + wall.getArea());
+
+        wall.setHeight(-1.5);
+
+        System.out.println("width= " + wall.getWidth());
+
+        System.out.println("height= " + wall.getHeight());
+
+        System.out.println("area= " + wall.getArea());
     }
 
 }
